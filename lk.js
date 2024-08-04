d502dd617ce788175c8960e282bf1eea328010e3
logeshkumar2563 <logeshkumar2563@gmail.com>
2024-08-04 10:26:02 +0530

    Add files via upload

---
+++ lk.js
@@ -0,0 +1,76 @@
+// let js1="cadd";
+// let js2=4;
+// let js3="true";
+// let js4=[1,2,3];
+// let js5={
+//     "fname":"lk",
+//     "age":20,
+//     "city":"dgl"
+// }
+// let js6=[
+//     {
+//     "stu":"aaa",
+//     "reg":202
+// },
+// {
+//     "stu":"bbb",
+//     "reg":204
+// },
+// {
+//     "stu":"eeee",
+//     "reg":203
+// }
+// ]
+// console.log(js6)
+// let js7=`[
+//     {
+//         "std":"lk",
+//         "reg":234
+//     },
+//     {
+//         "std":"ok",
+//         "reg":121
+//     },
+//     {
+//         "std":"sk",
+//         "reg":123
+//     }
+// ]`
+// console.log(js7)
+// console.log(typeof(js7))
+// let pursejs=JSON.parse(js7)
+// console.log(pursejs)
+
+// let.button=document.getElementById("button")
+// let.closebutton=document.getElementById("closebutton")
+// button.onclick=openwindow
+// closebutton.onclick=closewindow
+// let newwindow=null
+// function openwindow(){
+//     newwindow=window.open("link",null,null)
+// }
+// function closewindow(){
+//     newwindow.close()
+// }
+// consolecreate=document.getElementById("creatnew")
+// consoleresize=document.getElementById("resize")
+// consoleresize2=document.getElementById("resizewindow")
+
+// create.onclick=createnew;
+// resize.onclick=resizeby;
+// resize2.onclick=resizeto;
+// let newwindow=null;
+
+// function createnew(){
+//     newwindow=window.open(null,null,"width=50px,height=50px")
+// }
+// function resizeby(){
+//     newwindow=window.resizeBy(500px,500px)
+// }
+// function resizeto(){
+//     newwindow=window.resizeto(100,100)
+// }
+
+
+
+
---
+++ logeshjava.js
@@ -0,0 +1,109 @@
+// {
+//     var a=10;
+//     var b=20;
+// }
+// console.log(a+b)
+
+// var price=100;
+// var product=mobile;
+// var tax=12;
+// console.log(product)
+// console.log(price+tax)
+
+// export;1
+// var fru="apple"
+// var coun=10
+// var price=30
+// var tot=coun*price
+// console.log(fru)
+// console.log(tot)
+
+// // operator
+
+// var a=10
+// var b=--a
+// console.log(b)
+// decrement 
+// a=10;
+// b=++a;
+// console.log(b)
+// var a=10;
+// var b=10;
+// function add(){
+//     console.log(a+b);
+// }
+// add()
+// function
+
+// var fact="lk";
+// var fpla="dhoni";
+// var fmov="loe";
+
+// function lk(){
+//     console.log("fact,fpla,fmov")
+// }
+// lk()
+// ex2
+
+// var fname="lk";
+// var fage=20;
+
+// function ok(){
+//     console.log("name:"+fname);
+//     console.log("age:"+fage);
+
+
+// }
+// ok()
+
+// parameters function
+
+// function area(a,b)
+// {
+//     console.log(a*b)
+// }
+// area(10,3)
+let js1="cadd";
+let js2=4;
+let js3="true";
+let js4=[1,2,3];
+let js5={
+    "fname":"lk";
+    "age":20;
+    "city":"dgl"
+}
+let js6=[
+    {
+    "stu":"aaa";
+    "reg":202
+},
+{
+    "stu":"bbb";
+    "reg":204
+},
+{
+    "stu":"eeee";
+    "reg":203
+}
+]
+console.log(js6)
+console.log(js5)
+let js7=`[
+    {
+        "std":"lk",
+        "reg":234
+    },
+    {
+        "std":"ok",
+        "reg":121
+    },
+    {
+        "std":"sk",
+        "reg":123
+    }
+]`
+console.log(js7)
+
+console.log(print())
+
+
